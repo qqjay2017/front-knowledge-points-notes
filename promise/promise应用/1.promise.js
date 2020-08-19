c99d03483c066b33117f60735841ad457f5bd14d
huang <qqjay2013@gmail.com>
2020-08-19 11:01:01 +0800

    flutter

--- promise/promise应用/1.promise.js
+++ promise/promise应用/1.promise.js
@@ -1,7 +1,7 @@
 // es6规范中提供的一个类
 // 每个promise需要提供一个执行器函数(这个函数会立即执行)
 
-// const Promise = require('../手写promise/promise')
+const Promise = require('../手写promise/promise')
 
 
 
@@ -15,4 +15,6 @@ let promise1 = new Promise((resolve, reject) => { /* executor */
 promise1.then((value) => {
     console.log(value)
 })
+
+
 
--- promise/手写promise/promise.js
+++ promise/手写promise/promise.js
@@ -2,46 +2,48 @@ const PENDING = 'pending'; // 等待态
 const FULFILLED = 'fulfilled'; // 成功态
 const REJECTED = 'rejected'; // 失败态
 
-
-
-
 class Promise {
     constructor(executor) {
         // this 是类自己
+        this.state = PENDING; // 状态,默认等待态
+        this.value = undefined; // 成功的结果
+        this.reason = undefined; // 失败的原因
+
+        this.onFulfilled = []; //成功的回调
+        this.onRejected = []; // 失败的回调
+        // 成功的函数
+        let resolve = (value) => {
+            // must have a value
+            if (this.state == PENDING) {
+                this.state = FULFILLED;
+                this.value = value;
+                this.onFulfilled.forEach(fn => fn(value))
+            }
+        }
+        // 失败的函数
+        let reject = (reason) => {
+            // must have a reason
+            if (this.state == PENDING) {
+                this.state = REJECTED;
+                this.reason = reason;
+                this.onRejected.forEach(fn => fn(reason))
+            }
+        }
         try {
-            executor(this.resolve.bind(this), this.reject.bind(this))
+
+            // 执行器会立即执行
+            executor(resolve, reject)
         } catch (error) {
-            this.reject.call(this, error)
+            reject(error)
         }
     }
 
-    state = PENDING; // 状态,默认等待态
-    value = null; // 成功的结果
-    reason = null; // 失败的原因
-    onFulfilled = []; //成功的回调
-    onRejected = []; // 失败的回调
-    resolve(value) {
-        // this默认是undefined
-        // must have a value
-        if (this.state == PENDING) {
-            this.value = value;
-            this.state = FULFILLED;
-            this.onFulfilled.forEach(fn=>fn(this.value))
-        }
-    }
-    reject(reason) {
-        // must have a reason
 
-        if (this.state == PENDING) {
-            this.reason = reason;
-            this.state = REJECTED;
-            this.onRejected.forEach(fn=>fn(this.reason))
-        }
-    }
+
 
 
     then(onFulfilled, onRejected) {
-        
+
         if (this.state == FULFILLED) {
             typeof onFulfilled == 'function' && onFulfilled(this.value);
         }
@@ -51,11 +53,10 @@ class Promise {
         }
 
         // 处理异步逻辑
-        if(this.state == PENDING){
+        if (this.state == PENDING) {
             typeof onFulfilled == 'function' && this.onFulfilled.push(onFulfilled);
             typeof onFulfilled == 'function' && this.onRejected.push(onRejected);
         }
-
     }
 
 }
