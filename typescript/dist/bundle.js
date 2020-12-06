0e8006caf9df1b686ad3148e68e0208cd31b056f
huangSir <96051929@ccb.com>
2020-12-06 11:41:56 +0800

    11.函数的双向协变

--- typescript/dist/bundle.js
+++ typescript/dist/bundle.js
@@ -1,23 +1,98 @@
 (function () {
     'use strict';
 
-    // ts中的泛型
-    // 函数 接口 类型别名  
-    function createArray(times, val) {
-        var result = [];
-        for (var index = 0; index < times; index++) {
-            result.push(val);
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+    ***************************************************************************** */
+    /* global Reflect, Promise */
+
+    var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+
+    function __extends(d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    }
+
+    /**
+     * 原因:为了类型安全,为了使用时候不报错
+     * 返回值类型是协变的,而参数类型是逆变的
+     * 返回值类型可以传子类,参数可以传父类(参数只能少,返回值只能多)
+     */
+    // 例子
+    var Animal = /** @class */ (function () {
+        function Animal() {
         }
-        return result;
+        return Animal;
+    }());
+    var Dog = /** @class */ (function (_super) {
+        __extends(Dog, _super);
+        function Dog() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.name = "dog";
+            return _this;
+        }
+        return Dog;
+    }(Animal));
+    var WhiteDog = /** @class */ (function (_super) {
+        __extends(WhiteDog, _super);
+        function WhiteDog() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.age = 10;
+            return _this;
+        }
+        return WhiteDog;
+    }(Dog));
+    var BlackDog = /** @class */ (function (_super) {
+        __extends(BlackDog, _super);
+        function BlackDog() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.home = "beijing";
+            return _this;
+        }
+        return BlackDog;
+    }(Dog));
+    var dog = new Dog();
+    var whiteDog = new WhiteDog();
+    var blackDog = new BlackDog();
+    function exec(callback) {
+        //    callback(animal)     // error animal没有name属性
+        //    callback(WhiteDog)  // ok
+        callback(dog);
     }
-    var r1 = createArray(3, 'abc'); // <String> 不写,也会根据abc类型自动推导出泛型
-    console.log(r1[0].toLocaleLowerCase());
-    // 泛型可以使用多个
-    // 元组 [string,number] => [number,string]
-    function swap(tuple) {
-        return [tuple[1], tuple[0]];
-    }
-    console.log(swap(['aaa', 111]));
+    // exec(childToChild) // error
+    // ok
+    exec(function (blackDog) {
+        // blackDog 被转成了Dog类型
+        return blackDog; // 返回值也被装成了Dog
+    });
+    // exec(parentToParent)  // error
+    // ok,两个都变成了Dog
+    exec(function (animal) {
+        return animal;
+    });
+    var parentToChild = function (animal) { return blackDog; };
+    exec(parentToChild); // ok
+    exec(function (animal) {
+        console.log(animal);
+        return blackDog;
+    });
 
 }());
 //# sourceMappingURL=bundle.js.map
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,117 +1,83 @@
+/**
+ * 原因:为了类型安全,为了使用时候不报错
+ * 返回值类型是协变的,而参数类型是逆变的
+ * 返回值类型可以传子类,参数可以传父类(参数只能少,返回值只能多)
+ */
 
-// 1.接口的兼容性
-// 原理是  duck-check
+// 例子
 
-
-interface Animal {
-    name: string,
-    age: number
+class Animal {}
+class Dog extends Animal {
+  public name: string = "dog";
 }
 
-interface Person {
-    name: string,
-    age: number,
-    gender: number
+class WhiteDog extends Dog {
+  public age: number = 10;
 }
 
-
-function getName(animal: Animal): string {
-    return animal.name
+class BlackDog extends Dog {
+  public home: string = "beijing";
 }
 
+let animal: Animal = new Animal();
+let dog: Dog = new Dog();
+let whiteDog: WhiteDog = new WhiteDog();
+let blackDog: BlackDog = new BlackDog();
 
-let p: Person = {
-    name: 'zf',
-    age: 10,
-    gender: 0
-}
-// 这样是可以的,传参的时候每会进行兼容性比较,因为Animal的属性在Person里面都有
-getName(p);
+type Callback = (dog: Dog) => Dog;
 
-// 报错:赋值的时候不会比较,会直接报错
-// let a:Animal {
-//     gender:0
-// }
-
-
-// 2.基本数据类型的兼容性
-
-let numStr: string | number;
-let str: string = 'zf';
-// 可以,因为numStr可以容纳string和number
-numStr = str;
-
-
-let num2: {
-    toString(): string
-}
-let str2: string = 'jg';
-// 可以,因为str2有toString方法
-num2 = str2;
-
-
-// 3.类的兼容性
-
-namespace a {
-    class Animal { name: string; }
-    class Bird extends Animal { age: number }
-    let a: Animal;
-    let b: Bird = new Bird();
-    // a可以等于b,因为b继承a,有a所有属性
-    a = b;
-    // b不能等于a,因为Animal没有age属性
-    // b=a;
+function exec(callback: Callback): void {
+  //    callback(animal)     // error animal没有name属性
+  //    callback(WhiteDog)  // ok
+  callback(dog)
 }
 
-
-// 4.函数的兼容性 !!!!!
-// 1.比较参数2.比较返回值
-
-type Func = (a: number, b: number) => void;
-
-let sum:Func;
-
-function f1(a:number,b:number):void{
-
-}
-// 完全一样,可以
-sum = f1;
-
-function f2(a:number):void{
-
-}
-// 参数少一个:可以
-sum = f2;
-
-function f3(a:number,b:number,c:number):void{
-
-}
-// 多参数,不行
-// sum = f3;
+/**
+ * 4种情况
+ * 1.参数传子类,返回值子类   // 不可以
+ * 2.参数值子类,返回值父类   // 不可以
+ * 3.参数是父类,返回值是父类 // 不可以
+ * 4.参数是父类,返回值是子类  // 可以
+ */
 
 
-// 比较返回值
+// 1.参数传子类,返回值子类   // 不可以
+type ChildToChild = (blackDog: BlackDog) => BlackDog;
+let childToChild:ChildToChild;
+// exec(childToChild) // error
+// ok
+exec(function (blackDog) {
+    // blackDog 被转成了Dog类型
+  return blackDog; // 返回值也被装成了Dog
+});
 
-type GetPerson = ()=>{name:string,age:number}
-let getPerson :GetPerson;
-
-function g1(){
-    return {name:'zf',age:10}
-}
-// 可以,完全一样
-getPerson = g1;
-function g2(){
-    return {name:'zf'}
-}
-// 返回值少参数,不行
-// getPerson = g2;
-
-function g3(){
-    return {name:'zf',age:10,gender:0}
-}
-// 多参数,可以
-getPerson = g3;
+// 2.参数值子类,返回值父类   // 不可以
+type ChildToParent = (blackDog: BlackDog) => Animal;
+ let childToParent:ChildToParent;
+//  exec(childToChild) // error
+// error animal不是Dog的形状,没有name属性
+// exec(function(blackDog) {
+//     return animal
+// });
 
 
+// 3.参数是父类,返回值是父类 // 不可以
+type ParentToParent = (animal: Animal) => Animal;
+let parentToParent:ParentToParent = animal=>animal;
+// exec(parentToParent)  // error
 
-    export { }
+// ok,两个都变成了Dog
+exec(function(animal){  // 参数可以少
+    
+    return animal
+});
+
+// 4.参数是父类,返回值是子类  // 可以
+type ParentToChild = (animal: Animal) => BlackDog;
+let parentToChild:ParentToChild =(animal)=>blackDog;
+exec(parentToChild)  // ok
+exec(function(animal) {
+    console.log(animal)
+    return blackDog;
+} );
+export {};
