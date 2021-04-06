feb13ab360c83e2bdbedb131f976191588952efb
huangbo <johndoe@example.com>
2021-04-06 16:53:09 +0800

    先这样

--- VUE/simple-vben/src/layouts/default/components/Sidebar/SubMenu.vue
+++ VUE/simple-vben/src/layouts/default/components/Sidebar/SubMenu.vue
@@ -1,27 +1,40 @@
 <template>
-  <a-sub-menu :key="props.menuInfo.path" v-bind="$attrs">
+  <a-menu-item
+    :key="onlyOneChild.path"
+    v-if="hasChildren && hasChildren == 1"
+  >
+    <PieChartOutlined />
+    <span>{{ onlyOneChild.path }}</span>
+  </a-menu-item>
+  <a-sub-menu
+    :key="props.menuInfo.path"
+    v-bind="$attrs"
+    v-if="hasChildren && hasChildren > 1"
+  >
     <template #title>
       <span>
         <MailOutlined />
-        <span>{{ (props.menuInfo.meta &&  props.menuInfo.meta.title) || '1234' }}</span>
+        <span>{{
+         props.menuInfo.path
+        }}</span>
       </span>
     </template>
     <template v-for="item in props.menuInfo.children" :key="item.path">
       <template v-if="!item.children">
         <a-menu-item :key="item.path">
           <PieChartOutlined />
-          <span>{{ item.meta.title }}</span>
+          <span>{{ item.path }}</span>
         </a-menu-item>
       </template>
       <template v-else>
-        <sub-menu :menu-info="item" :key="item.key" />
+        <sub-menu :menu-info="item" :key="item.path" />
       </template>
     </template>
   </a-sub-menu>
 </template>
 
 <script lang="ts" setup name="SubMenu">
-import { defineProps } from "vue";
+import { computed, defineProps } from "vue";
 import { Menu } from "ant-design-vue";
 
 const ASubMenu = Menu.SubMenu;
@@ -31,11 +44,26 @@ import {
   MenuUnfoldOutlined,
   PieChartOutlined,
   MailOutlined,
-} from '@ant-design/icons-vue';
+} from "@ant-design/icons-vue";
 const props = defineProps({
   menuInfo: {
     type: Object,
     default: () => ({}),
   },
 });
+
+const hasChildren = computed(()=>{
+  if(!props.menuInfo.children || props.menuInfo.children.length == 0 ){
+    return false
+  }
+  return  props.menuInfo.children.length 
+})
+
+const onlyOneChild = computed(()=>{
+  if(!props.menuInfo.children || props.menuInfo.children.length == 0 ){
+    return false
+  }
+  return props.menuInfo.children[0]
+})
+
 </script>
--- VUE/simple-vben/src/layouts/default/components/Sidebar/index.vue
+++ VUE/simple-vben/src/layouts/default/components/Sidebar/index.vue
@@ -5,19 +5,19 @@
       theme="dark"
       mode="inline"
       :selectedKeys="selectedKeys"
-      :default-selected-keys="defaultSelectedKeys"
       @update:selectedKeys="changeSelectedKeys"
+     
     >
-    <template v-for="item in routes" :key="item.path">
-           <sub-menu :menuInfo="item"  />
-    </template>
+      <template v-for="item in routes" :key="item.path">
+        <sub-menu :menuInfo="item" />
+      </template>
     </Menu>
   </LayoutSider>
 </template>
 
 <script lang="ts" setup>
 import { Layout, Menu } from "ant-design-vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { computed, ref } from "vue";
 
 import { useStore } from "vuex";
@@ -25,25 +25,29 @@ const LayoutSider = Layout.Sider;
 const MenuItem = Menu.Item;
 
 import Logo from "./Logo.vue";
-import SubMenu from './SubMenu.vue'
+import SubMenu from "./SubMenu.vue";
 
 const store = useStore();
+
+const router = useRouter();
 
 const collapsed = computed(() => {
   return !!!store.state.app.sidebar.opened;
 });
 
-const selectedKeys = ref<string[]>(["1"]);
+const selectedKeys = ref<string[]>(['/']);
 
 const defaultSelectedKeys = ref<string[]>([]);
-const { fullPath } = useRoute()
+const { fullPath } = useRoute();
 
-defaultSelectedKeys.value = [fullPath]
-
+// defaultSelectedKeys.value = [fullPath];
+ selectedKeys.value = [fullPath]
 const changeSelectedKeys = (value: string[]) => {
-  console.log(value);
+  selectedKeys.value = value;
+  router.push(value[0]);
 };
 
-import {routes} from '/@/router/index'
-console.log(routes)
+import { routes } from "/@/router/index";
+
+
 </script>
