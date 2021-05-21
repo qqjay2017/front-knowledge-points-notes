3599eb99c65241d1dcc3ba1d2019295ca5bcb843
huangbo <johndoe@example.com>
2021-05-21 15:13:44 +0800

    feat(config): add svg icon component

---
+++ VUE/vue3/weypq3/brolly-vue3-element-admin-master/vue3-element-admin/src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Layout from '@/layout/index.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import(/* webpackChunkName: "dashboard" */ '@/views/dashboard/index.vue'),
+        meta: {
+          title: 'Dashboard'
+        }
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
