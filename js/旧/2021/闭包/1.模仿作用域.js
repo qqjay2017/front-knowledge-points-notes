cb06dda4ccad6de61075219babfb67946d10f063
huangbo <qqjay2013@gmail.com>
2023-09-04 11:45:21 +0800

    feat: 面试题11

--- js/旧/2021/闭包/1.模仿作用域.js
+++ js/旧/2021/闭包/1.模仿作用域.js
@@ -1,20 +1,19 @@
-function outputNumber(count){
-    for(var i=0;i<count;i++){
-        console.log(i)
+function outputNumber(count) {
+  for (var i = 0; i < count; i++) {
+    console.log(i);
+  }
+  // 变量i是定义在outputNumber的活动对象中,所以在for循环外也可以访问到
+  console.log(i, "out i");
+}
+
+function outputNumber2(count) {
+  (function () {
+    for (var i = 0; i < count; i++) {
+      console.log(i);
     }
-    // 变量i是定义在outputNumber的活动对象中,所以在for循环外也可以访问到
-    console.log(i,'out i')
+  })();
+  // 变量i是定义在匿名函数作用域里面,所以在for循环外访问不到
+  console.log(i, "out i");
 }
 
-function outputNumber2(count){
-    (function(){
-        for(var i=0;i<count;i++){
-            console.log(i)
-        }
-    })()
-    // 变量i是定义在outputNumber的活动对象中,所以在for循环外也可以访问到
-    console.log(i,'out i')
-}
-
-
-outputNumber(5)
+outputNumber(5);
--- js/旧/面试题/1.js
+++ js/旧/面试题/1.js
@@ -1,15 +1,13 @@
-function A (){
-    this.age = 18
+function A() {
+  this.age = 18;
 }
 
 // A.hello = console.log('hello')
 
+A.prototype.hello = function () {
+  console.log(this, "this");
+};
 
+let b = new A();
 
-A.prototype.hello = function(){
-    console.log(this)
-}
-
-let b = new A()
-
-b.hello()
+b.hello();
